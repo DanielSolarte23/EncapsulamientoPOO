a75fa5a90b9d45cffffe340522bc7458051baab2
DanielSolarte23 <royersolarte22@gmail.com>
2024-09-06 18:03:09 -0500

    Mongo snNm

--- ConexionMongo/routes/departamento.routes.js
+++ ConexionMongo/routes/departamento.routes.js
@@ -1,12 +1,12 @@
-import { obtenerDpto } from "../controllers/departamento.controllers.js";
+import { DptoC } from "../controllers/departamento.controllers.js";
 
 import express from 'express'
 import { Router } from 'express'
 
 const router = Router();
 
-router.get('/departamentos', obtenerDpto)
-// router.post('/departamentos', agregarDepartamentos)
+router.get('/departamentos', DptoC.obtenerDpto)
+router.post('/departamentos', DptoC.crearDpt)
 // router.put('/departamentos/:id', actualizarDepartamentos)
 // router.delete('/departamentos/:id', eliminarDepartamentos)
 
